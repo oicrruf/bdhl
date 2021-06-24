5641015f6b281ab5cb571186d1c3a28e5233a94e
Víctor M. Reyes <vm.reyesal@gmail.com>
2021-06-24 11:19:37 -0600

    feat: get products data for local json

--- src/app/products/products.page.ts
+++ src/app/products/products.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: 'products.page.html',
   styleUrls: ['products.page.scss']
 })
-export class ProductsPage {
+export class ProductsPage implements OnInit {
+  products: any;
+  movements: any;
+  constructor() { }
 
-  constructor() {}
+  ngOnInit() {
+    fetch('assets/data/products.json').then((res) => res.json()).then((json) => {
+      console.log(json);
+      this.products = json;
+    });
+
+    fetch('assets/data/movements.json').then((res) => res.json()).then((json) => {
+      console.log(json);
+      this.movements = json;
+    });
+  }
 
 }
